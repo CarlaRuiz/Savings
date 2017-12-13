5ba206bda9ed2b7c1f3d566695201c8a6d13ea09
Carla Ruiz <carlaruiz@Carlas-MacBook-Air.local>
2017-12-13 15:46:27 -0800

    Add stuff

--- Savings/ViewController.swift
+++ Savings/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var transactions: [Transaction] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func add(_ sender: Any) {
+        let transaction = Transaction(name: "Tesla", amount: 200000)
+        transactions.append(transaction)
+        tableView.reloadData()
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return transactions.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) 
+        -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = transactions[indexPath.row].name
+        return cell
+    }
 }
-
+class Transaction: NSObject {
+    var name: String
+    var amount: Int
+    
+    init(name:String, amount:Int){
+        self.name = name
+        self.amount = amount
+        super.init()
+        
+    }
+}
